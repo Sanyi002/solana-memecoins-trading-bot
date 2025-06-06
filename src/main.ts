5edb284d7e450b201e0eaadb48007bf8417b2432
Sandor Szabo <sanyi002@gmail.com>
2025-06-06 08:07:40 +0200

    feat: add pumpfun sdk

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,29 @@
-const main = () => {
-  console.log("Hello, World!");
+import { Connection, Keypair } from "@solana/web3.js";
+import dotenv from "dotenv";
+
+import { AnchorProvider, Wallet } from "@coral-xyz/anchor";
+import { PumpFunSDK } from "./pumpFunSDK";
+
+dotenv.config();
+
+const main = async () => {
+  console.log(process.env.HELIUS_RPC_URL);
+  let connection = new Connection(process.env.HELIUS_RPC_URL || "");
+
+  let wallet = new Wallet(new Keypair()); //note this is not used
+  const provider = new AnchorProvider(connection, wallet, {
+    commitment: "finalized",
+  });
+
+  const sdk = new PumpFunSDK(provider);
+
+  let tradeEvent = sdk.addEventListener(
+    "tradeEvent",
+    (event, slot, signature) => {
+      console.log("tradeEvent", event, slot, signature);
+    }
+  );
+  console.log("tradeEvent", tradeEvent);
 };
 
 main();
---
+++ src/pumpFunSDK.ts
@@ -0,0 +1,78 @@
+import { Program, Provider } from "@coral-xyz/anchor";
+import { IDL, PumpFun } from "./IDL";
+import { Connection } from "@solana/web3.js";
+import {
+  CompleteEvent,
+  CreateEvent,
+  PumpFunEventHandlers,
+  PumpFunEventType,
+  SetParamsEvent,
+  TradeEvent,
+} from "./types";
+import {
+  toCompleteEvent,
+  toCreateEvent,
+  toSetParamsEvent,
+  toTradeEvent,
+} from "./events";
+
+export class PumpFunSDK {
+  public program: Program<PumpFun>;
+  public connection: Connection;
+  constructor(provider?: Provider) {
+    this.program = new Program<PumpFun>(IDL as PumpFun, provider);
+    this.connection = this.program.provider.connection;
+  }
+
+  addEventListener<T extends PumpFunEventType>(
+    eventType: T,
+    callback: (
+      event: PumpFunEventHandlers[T],
+      slot: number,
+      signature: string
+    ) => void
+  ) {
+    return this.program.addEventListener(
+      eventType,
+      (event: any, slot: number, signature: string) => {
+        let processedEvent;
+        switch (eventType) {
+          case "createEvent":
+            processedEvent = toCreateEvent(event as CreateEvent);
+            callback(
+              processedEvent as PumpFunEventHandlers[T],
+              slot,
+              signature
+            );
+            break;
+          case "tradeEvent":
+            processedEvent = toTradeEvent(event as TradeEvent);
+            callback(
+              processedEvent as PumpFunEventHandlers[T],
+              slot,
+              signature
+            );
+            break;
+          case "completeEvent":
+            processedEvent = toCompleteEvent(event as CompleteEvent);
+            callback(
+              processedEvent as PumpFunEventHandlers[T],
+              slot,
+              signature
+            );
+            break;
+          case "setParamsEvent":
+            processedEvent = toSetParamsEvent(event as SetParamsEvent);
+            callback(
+              processedEvent as PumpFunEventHandlers[T],
+              slot,
+              signature
+            );
+            break;
+          default:
+            console.error("Unhandled event type:", eventType);
+        }
+      }
+    );
+  }
+}
